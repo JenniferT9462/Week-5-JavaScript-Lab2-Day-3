7f14a02adca965965d4ed82e730ebe0f1186c0c0
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-06-21 14:22:33 -0500

    add style change css on webpage

--- script.js
+++ script.js
@@ -6,19 +6,37 @@ function startQuest() {
     if(game.choice1 === "left") {
         game.choice2 = prompt("You come to a lake, there is a castle in the middle of the lake. Do you 'wait' and look for a boat or 'swim' across? Type wait or swim. ");
     }else if(game.choice1 === "right") {
+        gameHeader2.innerHTML = "Game Over";
         gameParagraph.innerHTML = "You fell in a hole. Game Over!";
+        gameParagraph.style.backgroundColor = "black";
+        gameParagraph.style.color = "red"
     }
     if(game.choice2 === "wait") {
         game.choice3 = prompt("You find a raft and get to the castle unharmed. Inside the castle there are 3 doors; gold door, purple door and a green door. Which color do you choose?");
     }else if(game.choice2 === "swim") {
+        gameHeader2.innerHTML = "Game Over";
         gameParagraph.innerHTML = "You were eaten by a shark. Game Over!";
+        gameParagraph.style.backgroundColor = "navy";
+        gameParagraph.style.color = "red";
     }
     if(game.choice3 === "gold") {
-        gameParagraph.innerHTML = `You chose the ${game.choice3} door. You were cooked and eaten by the dragon!!`;
+        gameHeader2.innerHTML = "Game Over";
+        gameParagraph.innerHTML = `You chose the <strong>${game.choice3}</strong> door. You were cooked and eaten by the dragon!!`;
+        gameParagraph.style.backgroundColor = "darkgreen";
+        gameParagraph.style.color = "red"
     }else if(game.choice3 === "green") {
-        gameParagraph.innerHTML = `You chose the ${game.choice3} door. You fell into a fiery pit!!`;
+        gameHeader2.innerHTML = "Game Over";
+        gameParagraph.innerHTML = `You chose the <strong>${game.choice3}</strong> door. You fell into a fiery pit!!`;
+        gameParagraph.style.backgroundColor = "brown";
+        gameParagraph.style.color = "red"
     }else if(game.choice3 === "purple") {
-        gameParagraph.innerHTML = `You chose the ${game.choice3} door. You found the princess and together you defeated the dragon!`;
+        gameHeader2.innerHTML = "Victory";
+        gameParagraph.innerHTML = `You chose the <strong>${game.choice3}</strong> door. You found the princess and together you defeated the dragon!`;
+        gameParagraph.style.backgroundColor = "purple";
+        gameParagraph.style.color = "gold";
+        gameHeader2.style.backgroundColor = "gold";
+        gameHeader2.style.color = "purple";
+        
     }
         
 
